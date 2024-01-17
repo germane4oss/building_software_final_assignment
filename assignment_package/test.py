b21f649768178102ffaf1cfc963e92697817ea53
German Silva <german@e4oss.ca>
2024-01-17 13:01:06 -0500

    test cases

--- assignment_package/test.py
+++ assignment_package/test.py
@@ -1,8 +1,13 @@
-class Student:
-    def __init__(self, name, age): 
-        self.name = name
-        self.age = age
+from assignment_package import Analysis
 
-std = Student('Bill',25) #passing values to constructor
-print(std.name)
-print(std.age)
+#create an Analysis class instance
+analysis_1 = Analysis('configs/job_config.yml')
+
+# load github repositories data
+analysis_1.load_data()
+
+# get the top 30 repositories based on the number of stars
+analysis_1.compute_analysis()
+
+# plot the data
+analysis_1.plot_data()
